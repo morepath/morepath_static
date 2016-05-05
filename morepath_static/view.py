a4d3c597c2d554efd07180a7d728c39f92bd16e6
Henri Hulski <henri.hulski@gazeta.pl>
2016-05-05 22:32:49 +0200

    Move static_components to static.py

--- morepath_static/view.py
+++ morepath_static/view.py
@@ -1,6 +1,3 @@
-import bowerstatic
-import os
-
 from .model import Root
 from .app import App
 
@@ -10,22 +7,3 @@ def root_default(self, request):
     request.include('my_component')
     return ("<!DOCTYPE html><html><head></head><body>"
             "components are inserted in the HTML source</body></html>")
-
-
-bower = bowerstatic.Bower()
-
-
-components = bower.components(
-    'app', os.path.join(os.path.dirname(__file__), 'bower_components'))
-
-
-local = bower.local_components('local', components)
-
-
-local.component(os.path.join(os.path.dirname(__file__), 'my_component'),
-                version=None)
-
-
-@App.static_components()
-def get_static_components():
-    return local
