f7b2cab43da1f4b0de8cc1e8c4438932330aa0d6
Jürgen Gmach <juergen.gmach@googlemail.com>
2021-01-12 18:16:02 +0100

    use GitHub Actions instead of Travis CI
    ... as Travis does no longer support open source projects.
    
    Also:
    - add support for Python 3.9
    - use pre-commit
    - use pyupgrade
    - use black

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,8 +4,14 @@ CHANGES
 0.2 (unreleased)
 ================
 
-- Make Python 3.8 the default testing environment
+- Make Python 3 the default testing environment
 - Drop support for Python versions < 3.6
+- Add support for Python 3.9
+- Use GitHub Actions instead of Travis CI
+(Travis stopped supporting open source projects).
+- use pre-commit
+- use pyupgrade
+- use black
 
 
 0.1 (2016-07-06)
--- morepath_static/tests/test_morepath_static.py
+++ morepath_static/tests/test_morepath_static.py
@@ -6,21 +6,22 @@ from ..__main__ import run
 def test_static_assets():
     c = Client(App())
 
-    r = c.get('/')
+    r = c.get("/")
 
-    scripts = r.html.select('script')
+    scripts = r.html.select("script")
     assert len(scripts) == 2
 
     for s in scripts:
-        c.get(s['src'])
+        c.get(s["src"])
 
 
 def test_run(monkeypatch):
     import morepath
+
     instances = []
-    monkeypatch.setattr(morepath, 'run', lambda app: instances.append(app))
+    monkeypatch.setattr(morepath, "run", lambda app: instances.append(app))
 
     run()
 
-    app, = instances
+    (app,) = instances
     assert isinstance(app, App)
